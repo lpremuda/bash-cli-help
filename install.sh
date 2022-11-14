b263b07cc3effcc9f529321c2b98896f992ce33f
Lucas Premuda <lucas.premuda@procore.com>
2022-11-13 21:38:26 -0800

    Added alias file installation with edit aliases

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+ALIAS_FILENAME="cli_help_aliases.zsh"
+ALIAS_FILENAME_BAK="${ALIAS_FILENAME}.bak"
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 chmod +x ${SCRIPT_DIR}/shared/load_shared_resources.sh
 source ${SCRIPT_DIR}/shared/load_shared_resources.sh
@@ -13,25 +16,31 @@ function echoCurrStep() {
     sleep 1
 }
 
-ALIAS_FILENAME="cli_help_aliases.zsh"
-ALIAS_FILENAME_BAK="${ALIAS_FILENAME}.bak"
-echoCurrStep "Moving ${ALIAS_FILENAME} to ${HOME}/.oh-my-zsh/custom"
-if [[ -f ${HOME}/.oh-my-zsh/custom/${ALIAS_FILENAME} ]]; then
-	echo -e "${YELLOW}${HOME}/.oh-my-zsh/custom/${ALIAS_FILENAME} already exists. Creating backup file: ${ALIAS_FILENAME_BAK}${NOCOLOR}" 
-	mv ${HOME}/.oh-my-zsh/custom/${ALIAS_FILENAME} ${HOME}/.oh-my-zsh/custom/${ALIAS_FILENAME_BAK}
-	echo -e "\t${GREEN}Backing up ${HOME}/.oh-my-zsh/custom/${ALIAS_FILENAME} complete!${NOCOLOR}\n"
-fi
-echo -e "Moving ${ALIAS_FILENAME} to ${HOME}/.oh-my-zsh/custom\n"	
-mv ${SCRIPT_DIR}/${ALIAS_FILENAME} ${HOME}/.oh-my-zsh/custom
-echo "Done!"
+echo -e -n "\n" >> ${ALIAS_FILENAME}
+
+function moveAliasFile() {
+	echoCurrStep "Moving ${ALIAS_FILENAME} to ${HOME}/.oh-my-zsh/custom"
+	if [[ -f ${HOME}/.oh-my-zsh/custom/${ALIAS_FILENAME} ]]; then
+		echo -e "${YELLOW}${HOME}/.oh-my-zsh/custom/${ALIAS_FILENAME} already exists. Creating backup file: ${ALIAS_FILENAME_BAK}${NOCOLOR}" 
+		mv ${HOME}/.oh-my-zsh/custom/${ALIAS_FILENAME} ${HOME}/.oh-my-zsh/custom/${ALIAS_FILENAME_BAK}
+		echo -e "\t${GREEN}Backing up ${HOME}/.oh-my-zsh/custom/${ALIAS_FILENAME} complete!${NOCOLOR}\n"
+	fi
+	echo -e "Moving ${ALIAS_FILENAME} to ${HOME}/.oh-my-zsh/custom\n"	
+	mv ${SCRIPT_DIR}/${ALIAS_FILENAME} ${HOME}/.oh-my-zsh/custom
+	echo "Done!"
+}
 
 for sh_absolute_path in $(ls ${SCRIPT_DIR}/*_help.sh); do
+	# Get basename from absolute path
 	sh_basename=$(basename $sh_absolute_path)
 	echoCurrStep "Processing shell script: $sh_basename"
+
+	# Make shell script executable
 	echo -e "Making ${sh_basename} executable"
 	chmod +x ${SCRIPT_DIR}/${sh_basename}
 	echo -e "\t${GREEN}${sh_basename} is executable!${NOCOLOR}\n"
 
+	# Create symbolic link
 	arr=(${sh_basename//_/ })
 	binary=${arr[0]}
 	sym_link_basename=${binary}h
@@ -42,7 +51,15 @@ for sh_absolute_path in $(ls ${SCRIPT_DIR}/*_help.sh); do
 		ln -s ${sh_absolute_path} ${sym_link_basename}	
 		echo -e "\t${GREEN}${sym_link_basename} symbolic link created!${NO_COLOR}"
 	fi
+
+	# Create alias to edit shell script in ${ALIAS_FILENAME}
+	edit_alias_name="${sym_link_basename}e"
+	echo "alias ${edit_alias_name}='nvim \${CLI_HELP_DIR}/${sh_basename}'" >> ${ALIAS_FILENAME}
 done
+
+echo -e -n "\n" >> ${ALIAS_FILENAME}
+
+moveAliasFile
 
 echoCurrStep "Adding ${SCRIPT_DIR} to PATH env variable"
 echo "Checking if ${SCRIPT_DIR} is already on PATH"
