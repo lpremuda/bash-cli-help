65d3286ad5508ea3eb52d079593823a540ca20c9
Lucas Premuda <lucas.premuda@procore.com>
2023-05-25 10:48:48 -0700

    Added kcu alias (kubectl config use-context)

--- cli_help_aliases.zsh
+++ cli_help_aliases.zsh
@@ -11,6 +11,7 @@ alias ia='iac-ops'
 alias k='kubectl'
 alias kcg='kubectl config get-contexts'
 alias kcs='kubectl config set-context'
+alias kcu='kubectl config use-context'
 alias kn='f() { kubectl config set-context --current --namespace=$1 };f'
 
 alias mang='f() { man $1 | col -b | grep -A10 -- $2 };f'
