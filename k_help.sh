2e6da9f6b57af1a120f53098e9405cdc357e7a90
Lucas Premuda <lucas.premuda@procore.com>
2023-05-25 17:08:33 -0700

    Modified k_help.sh

--- k_help.sh
+++ k_help.sh
@@ -5,17 +5,16 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source ${SCRIPT_DIR}/shared/load_shared_resources.sh
 
 printTitle "kubectl config"
-echo "kubectl config get-contexts"
-echo -e "\talias: kcg"
-echo "kubectl config use-context [context_name]"
-echo "kubectl config set-context ..."
-echo -e "\talias: kcs ..."
-echo "kubectl config set-context [--current] [--namespace=<namespace>]"
-echo -e "\talias: kn <namespace>"
+echo -e "kubectl config get-contexts                                       alias: kcg"
+echo -e "kubectl config use-context <context_name>                         alias: kcu"
+echo -e "kubectl config set-context                                        alias: kcs"
+echo -e "kubectl config set-context [--current] [--namespace=<namespace>]  alias: kn <namespace>"
 
 printTitle "kubectl exec"
-echo "kubectl exec -it <pod_name> [-c container_name] -- /bin/bash"
-echo "kubectl exec -it deployment/<deployment_name> -- /bin/bash"
+echo -e "kubectl exec -it <pod_name> [-c container_name] -- /bin/bash"
+echo -e "kubectl exec -it deployment/<deployment_name> -- /bin/bash"
 
+printTitle "VirtualService"
+echo -e "kubectl get vs -A -o jsonpath='{.items[*].spec.http[*].route[*].destination.port.number}'"
 
 echo ""
