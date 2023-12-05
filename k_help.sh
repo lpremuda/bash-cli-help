c2f3ac448a8bd1282d321c6081a5b2f9266273b5
Lucas Premuda <lucas.premuda@procore.com>
2023-12-05 13:59:09 -0800

    Added create Job from CronJob in k_help.sh

--- k_help.sh
+++ k_help.sh
@@ -14,6 +14,10 @@ printTitle "kubectl exec"
 echo -e "kubectl exec -it <pod_name> [-c container_name] -- /bin/bash"
 echo -e "kubectl exec -it deployment/<deployment_name> -- /bin/bash"
 
+printTitle "Create Job from CronJob"
+echo -e "kubectl create job --from=cronjob/[CRONJOB_NAME] [JOB_NAME]"
+echo -e "\tkubectl create job --from=cronjob/nobl9-collector nobl9-collector"
+
 printTitle "VirtualService"
 echo -e "kubectl get vs -A -o jsonpath='{.items[*].spec.http[*].route[*].destination.port.number}'"
 
