04423f4a1976b9d9fbde2f511e9104200cbd8952
Lucas Premuda <lucas.premuda@procore.com>
2023-12-05 22:14:37 -0800

    Added S3 command to aws_help.sh

--- aws_help.sh
+++ aws_help.sh
@@ -5,16 +5,20 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source ${SCRIPT_DIR}/shared/load_shared_resources.sh
 
 printTitle "SSO"
-echo "aws sso login [--profile default]"
-echo "aws sso login --profile stg"
-echo "aws sts get-caller-identity	alias: asg"
-echo "export AWS_PROFILE=<profile>"
-echo "unset AWS_PROFILE"
+echo -e "aws sso login [--profile default]"
+echo -e "aws sso login --profile stg"
+echo -e "aws sts get-caller-identity	alias: asg"
+echo -e "export AWS_PROFILE=<profile>"
+echo -e "unset AWS_PROFILE"
 
-printTitle "cat ~/.aws/config"
-cat ${HOME}/.aws/config
+printTitle "S3"
+echo -e "aws s3 ls [S3_URI] [--recursive]"
+echo -e "\taws s3 ls s3://procore-app-dlh-staging-us-east-1/external --recursive"
 
 printTitle "Route53"
 echo -e "aws route53 list-hosted-zones"
 
-echo ""
+printTitle "cat ~/.aws/config"
+cat ${HOME}/.aws/config
+
+echo -e ""
