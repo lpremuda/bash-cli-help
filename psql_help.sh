c52772825481f9c1c16a0e20b0d99cecbb83c4eb
Lucas Premuda <lucas.premuda@procore.com>
2024-01-30 15:01:46 -0800

    Added to psql_help.sh

--- psql_help.sh
+++ psql_help.sh
@@ -17,14 +17,17 @@ printTitle "Schema commands:"
 echo -e "${GREEN}\\dn${NOCOLOR}            List schemas"
 
 printTitle "Table commands:"
-echo -e "${GREEN}\\dt${NOCOLOR}                   List tables from current schema"
-echo -e "${GREEN}\\dt *.*${NOCOLOR}               List tables from all schemas"
-echo -e "${GREEN}\\dt <SCHEMA>.*${NOCOLOR}        List tables from schema <SCHEMA>"
+echo -e "${GREEN}\dt${NOCOLOR}                    List tables from current schema"
+echo -e "${GREEN}\dt *.*${NOCOLOR}                List tables from all schemas"
+echo -e "${GREEN}\dt <SCHEMA>.*${NOCOLOR}         List tables from schema <SCHEMA>"
 echo -e "${GREEN}DROP TABLE <TABLE>;${NOCOLOR}    Delete table <TABLE> (use quotes for mixed-case names)"
 echo -e "${GREEN}SELECT * FROM <TABLE>;${NOCOLOR} Select all columns in <TABLE>"
 echo -e "${GREEN}TABLE <TABLE>;${NOCOLOR}         Select all columns in <TABLE>"
 echo -e "${GREEN}TRUNCATE <TABLE> [CASCADE];${NOCOLOR} Delete all entries in <TABLE> and, if CASCADE, delete relational entries in other tables"
 
-
+printTitle "Sequence commands:"
+echo -e "${GREEN}\ds${NOCOLOR}                       List sequences"
+echo -e "${GREEN}SELECT * FROM <SEQUENCE>${NOCOLOR}  Select all columns in <SEQUENCE>"
+echo -e "${GREEN}ALTER SEQUENCE <SEQUENCE> RESTART WITH <NUMBER>${NOCOLOR} Set next sequence number to <NUMBER>"
 
 echo -e ""
